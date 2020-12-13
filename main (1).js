83c672719f4336c515e866fd5f139dc6ff14bcc2
Jaygopal-Lenka <73235238+Jaygopal-Lenka@users.noreply.github.com>
2020-12-13 11:25:08 +0530

    Add files via upload

--- main (1).js
+++ main (1).js
@@ -36,3 +36,86 @@ function new_image(get_image) {
     canvas.add(block_object);
 });
 }
+
+window.addEventListener("keydown",my_keydown);
+
+function my_keydown(e){
+    keyPressed = e.keyCode;
+
+    if (e.shiftKey== true && keyPressed=="80") {
+
+        block_image_width=block_image_width+10;
+        block_image_height=block_image_height+10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+
+    if(e.shiftKey== true && keyPressed =="77") {
+        block_image_width=block_image_width-10;
+        block_image_height=block_image_height-10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+
+    if (keyPressed=='70'){
+        new_image("ironman_face.png");
+    }
+    if (keyPressed=='66'){
+        new_image("spiderman_body.png");
+    }
+    if (keyPressed=='76'){
+        new_image("hulk_legs.png");
+    }
+    if (keyPressed=='70'){
+        new_image("thor_right_hand.png");
+    }
+    if (keyPressed=='72'){
+        new_image("captain_america_left_hand.png");
+    }
+    if(keyPressed=='38'){
+        up();
+    }
+    if(keyPressed=='40'){
+        down();
+    }
+    if(keyPressed=='37'){
+        left();
+    }
+    if(keyPressed=='39'){
+        right();
+    }
+
+}
+function up() {
+     
+    if (player_y>0) {
+        player_y= player_y- block_image_height;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down() {
+     
+    if (player_y<=500) {
+        player_y= player_y+ block_image_height;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left() {
+     
+    if (player_x>0) {
+        player_x= player_x- block_image_width;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right() {
+     
+    if (player_y<=850) {
+        player_x= player_x+ block_image_width;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
